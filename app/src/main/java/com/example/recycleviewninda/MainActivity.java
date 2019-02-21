dcd321241337ce67e668227f7c2a0f55d73650f8
Ninda Sabilla <ninda.sabilla123@gmail.com>
2019-02-21 11:01:55 +0700

    membuat RecycleView

---
+++ app/src/main/java/com/example/recycleviewninda/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.recycleviewninda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.recycleviewninda.adapter.ContactsAdapter;
+import com.example.recycleviewninda.models.Contact;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Contact> contacts;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView rv = findViewById(R.id.rvContacts);
+        contacts = Contact.createContactsList(20);
+        ContactsAdapter contactsAdapter = new ContactsAdapter(contacts);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        rv.setAdapter(contactsAdapter);
+        rv.setLayoutManager(layoutManager);
+
+    }
+}
